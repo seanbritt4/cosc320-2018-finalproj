b909093ebabd2733455fa2e2eac646d0d3a616a1
seanbritt4 <seanbritt4@gmail.com>
2018-11-25 06:33:26 -0500

    converted from C++ to C & more

--- arr.h
+++ arr.h
@@ -1,39 +1,109 @@
-/*
-    could move to main but made it seperate incase we need to add to it
-    
-
-
-*/
-
 #ifndef ARR_H
 #define ARR_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+// #include <unistd.h>
+
+//global int- one million
+int oneMil = 15;//1000000;
 
 
-//  params: n- size of the arr (could remove if we decide to standardize the size)
-//  generates n random integers (0-999,999) and populates array 'arr'
-//  returns array
-int* genArr(int n)
+// writes array to screen
+// currently unused
+void printArr(int *arr)
+{
+    int i;
+    for(i =0; i<oneMil; i++){
+        if(i == oneMil - 1)  printf("%d: %d\n", i, arr[i]);      //formatting, writes new line instead of ,
+        else        printf("%d: %d, ", i, arr[i]);               //formatting, writes , instead of new line
+
+    }
+}
+
+//writes array to FILE passed 
+// TODO- test for 1,000,000... file too large for c9 lol
+//     - works for 50,000
+void writeArr(int* arr, char* file)
+{
+    
+    umask(0);
+    FILE* des = fopen(file, "w");    //using FOPEN to use FPRINTF
+    
+    // fopen validation - remove if not wanted/needed
+    if(des < 0)
+    {
+        printf("Open File Error\n");
+        exit(0);
+    }
+    
+    lseek(des, 0, SEEK_SET);
+    int i;
+    printf("Writing to \"%s...\"\n", file);
+    for(i =0; i<oneMil; i++)
+    {
+        lseek(des, 0, SEEK_CUR);            
+        char str[20];                       //string buffer
+        // str[0] = 'd';                       //stack overflow said to initialize first ele. but seems to work lol
+        sprintf(str, "%d: %d", i, arr[i]);  //format string to print
+        fprintf(des, "%s\n", str);          //print str to file
+    }    
+    
+    fclose(des);
+    printf("Done\n");   
+}
+
+
+// takes an array as argument named arr
+// randomize order of array passed as argument
+//  return randomized array
+int* shuffle(int *arr)
 {
     srand(time(NULL));
+    printf("Shuffling array...");
+    int i = 0, count = 0;
+    
 
-    int* arr = new int[n];
-    for(int i = 0; i < n; i++)
+    for(;i<oneMil * 10; i++)
     {
-        arr[i] = rand()% 100;
+        int temp;
+        int a = arr[rand() % oneMil];
+        int b = arr[rand() % oneMil];
+        
+        temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
-    
+
+    printf(" Done\n");
+    writeArr(arr, "shuf.txt");
+
     return arr;
 }
 
-void printArr(int *arr, int n){
-    for(int i =0; i<n; i++){
-        std::cout << i << ": "<< arr[i] << std::endl;
+// generates 1,000,000 element array with numbers 0-999,999 in DESCENDING order
+//  then calls shuffle() to  the array
+//  returns array
+int* genArr()
+{
+    printf("Generating array...");
+    int i;
+    int *arr = malloc(oneMil * sizeof(arr));
+
+    for(i = 0; i < oneMil; i++)
+    {
+        arr[i] = oneMil - i;
     }
+    
+    printf(" Done\n");
+    writeArr(arr, "orig.txt");
+    return shuffle(arr);
 }
+
 
 #endif
--- deSort.h
+++ deSort.h
@@ -1,43 +1,23 @@
 #ifndef DESORT_H
 #define DESORT_H
 
-/*
-    params: arr- array to be sorted
-            n- size of 'arr'
-    
-    performs serial(standard) double ended selection sort on 'arr'
-    
-    returns: void-
-*/
-int* deSort(int a[],int n){
-// 	printf("\nsorting...\n");
-	
-	for(int i = 0; i < (n-1)/2; i++){
-		int min = i,
-			max = n-1-i;
-		
-		for(int j = i+1; j < n-i; j++){
-			if(a[j] < a[min])
-				min = j;
-				
-			if(a[j] > a[max])
-				max = j;
-		}
+#include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <pthread.h>
 
-		if(min != i){
-			int temp = a[i];
-			a[i] = a[min];
-			a[min] = temp;
-		}	
-		
-		if(max != n-1-i){
-			int temp = a[n-1-i];
-			a[n-1-i] = a[max];
-			a[max] = temp;
-		}
-	}
-     return a;
+void *frontEnd()
+{
+	sleep(1);
+	printf("in front end");
+	return NULL;
 }
 
+void *backEnd()
+{
+	sleep(1);
+	printf("in front end");
+	return NULL;
+}
 
 #endif
